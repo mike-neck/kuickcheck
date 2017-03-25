67ae8aaf3ce06ee1e1487ce7f4bd3d4d3abd225e
ornoyman <or.noyman@citi.com>
2017-03-25 09:37:52 +0300

    mark enum type as 'out'

--- core/src/main/kotlin/org/mikeneck/kuickcheck/generator/EnumGenerator.kt
+++ core/src/main/kotlin/org/mikeneck/kuickcheck/generator/EnumGenerator.kt
@@ -3,7 +3,7 @@ package org.mikeneck.kuickcheck.generator
 import org.mikeneck.kuickcheck.Generator
 import org.mikeneck.kuickcheck.random.RandomSource
 
-class EnumGenerator<E : Enum<E>>(val values: List<E>) : Generator<E> {
+class EnumGenerator<out E : Enum<out E>>(val values: List<E>) : Generator<E> {
     override fun invoke(): E {
         val x = RandomSource.nextInt(values.size)
         return values[x]
