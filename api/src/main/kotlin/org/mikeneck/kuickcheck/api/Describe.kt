c2407c4d226694aebbc89d2bfa4b67e8e85f2686
mike_neck <jkrt3333@gmail.com>
2017-06-08 00:15:58 +0900

    KC-5 move Testables to the other file.

--- api/src/main/kotlin/org/mikeneck/kuickcheck/api/Describe.kt
+++ api/src/main/kotlin/org/mikeneck/kuickcheck/api/Describe.kt
@@ -20,36 +20,6 @@ abstract class Describe(val description: String, val optionConfig: Nothing) {
     abstract val check: Testable
 }
 
-sealed class Testable {
-    abstract val size: Int
-    abstract infix operator fun plus(other: Testable): Testable
-}
-
-internal object NoTests : Testable() {
-    override val size: Int = 0
-    override fun plus(other: Testable): Testable = other
-}
-
-internal class SingleTest<A>(
-        val title: String,
-        val gen: () -> Gen<A>,
-        val property: (A) -> Boolean) : Testable() {
-    override val size: Int = 1
-    override fun plus(other: Testable): Testable = when (other) {
-        is NoTests -> this
-        is SingleTest<*> -> TestList(2, listOf(this, other))
-        is TestList -> TestList(other.size + 1, other.tests + this)
-    }
-}
-
-internal class TestList(override val size: Int, val tests: List<Testable>) : Testable() {
-    override fun plus(other: Testable): Testable = when (other) {
-        is NoTests -> this
-        is SingleTest<*> -> TestList(size + other.size, tests + other)
-        is TestList -> TestList(size + other.size, tests + other.tests)
-    }
-}
-
 object KuickCheckApi {
 
     fun prop(title: String): Qualifier = object : Qualifier {
